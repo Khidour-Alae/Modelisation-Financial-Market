841aa12a319f56b237282bfdf12269897fd7c6c5
hugo picard <hugo@hugop>
2022-04-11 15:47:44 +0200

    Tout marche normalement

--- project.py
+++ project.py
@@ -98,8 +98,6 @@ pricer_2(3,0.02,0.05,-0.05,100,f)
 
 
 def compare(p1, p2, N, rn, hn, bn, s, f) :
-    print (p1(N, s, rn, hn, bn, f))
-    print(p2(N, rn, hn, bn, s, f))
     return p1(N, s, rn, hn, bn, f) - p2(N, rn, hn, bn, s, f)
 
 compare(pricer_1, pricer_2, np.random.randint(5,16), 0.01, 0.05, -0.05, 100, f)
@@ -110,10 +108,47 @@ compare(pricer_1, pricer_2, np.random.randint(5,16), 0.01, 0.05, -0.05, 100, f)
 ###################################################################
 
 
-num0_a = f2((1 + 0.05) * 100) - f2((1 - 0.05) * 100)
+def pricer_2_bis(N, rn, hn, bn, s, f, e): #cette fonction a un paramètre en plus par rapport à pricer_2, le paramètre en plus est e qui correspond à l'étape à laquelle nous voulons nous arréter
+    qn = (rn - bn)/(hn - bn)
+
+    St_N = getSt_N(N,hn,bn,s)
+    Vk = f(St_N) #VN
+    for n in range(N,e,-1):
+        global Vkmoins1
+        Vkmoins1 = np.zeros(n)
+        for k in range(0,n): #on parcourt Vk
+            Vkmoins1[k] = (qn * Vk[k] + (1-qn) * Vk[k+1]) / (1 +rn) # Vkmoins1 = (qn * Vk((1+hn)*St_kmoins1) + (1-qn) * Vk((1+bn)*St_kmoins1) ) / (1 + rn)
+        Vk = Vkmoins1
+    return Vkmoins1
+
+
+num0_a = pricer_2_bis(2, 0.03, 0.05, -0.05, 100, f, 1)[0] - pricer_2_bis(2, 0.03, 0.05, -0.05, 100, f, 1)[1]
 den0_a = 100 * (0.05 + 0.05)
 alpha0 = num0_a / den0_a
 
-num0_b = f2((1 - 0.05) * 100) * (1 + 0.05) - f2((1 + 0.05) * 100) * (1 - 0.05)
+num0_b = pricer_2_bis(2, 0.03, 0.05, -0.05, 100, f, 1)[1] * (1 + 0.05) - pricer_2_bis(2, 0.03, 0.05, -0.05, 100, f, 1)[0] * (1 - 0.05)
 den0_b = (0.05 + 0.05) * (1 + 0.03)
 beta0 = num0_b / den0_b
+
+
+num1_a_1 = f((1 + 0.05) * getSt_N(1, 0.05, -0.05, 100)[0]) - f((1 - 0.05) * getSt_N(1, 0.05, -0.05, 100)[0])
+den1_a_1 = 100 * (0.05 + 0.05)
+alpha1_1 = num1_a_1 / den1_a_1
+
+num1_a_2 = f((1 + 0.05) * getSt_N(1, 0.05, -0.05, 100)[1]) - f((1 - 0.05) * getSt_N(1, 0.05, -0.05, 100)[1])
+den1_a_2 = 100 * (0.05 + 0.05)
+alpha1_2 = num1_a_2 / den1_a_2
+
+alpha1 =  np.array([alpha1_1, alpha1_2])
+
+
+num1_b_1 = f((1 - 0.05) * getSt_N(1, 0.05, -0.05, 100)[0]) * (1 + 0.05) - f((1 + 0.05) * getSt_N(1, 0.05, -0.05, 100)[0]) * (1 - 0.05)
+den1_b_1 = (0.05 + 0.05) * (1 + 0.03)
+beta1_1 = num1_b_1 / den1_b_1
+
+
+num1_b_2 = f((1 - 0.05) * getSt_N(1, 0.05, -0.05, 100)[1]) * (1 + 0.05) - f((1 + 0.05) * getSt_N(1, 0.05, -0.05, 100)[1]) * (1 - 0.05)
+den1_b_2 = (0.05 + 0.05) * (1 + 0.03)
+beta1_2 = num1_b_2 / den1_b_2
+
+beta1 = np.array([beta1_1, beta1_2])
