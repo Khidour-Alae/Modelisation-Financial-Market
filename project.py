d44e4e196078d1c67fd0135702f3cbd517c3570b
hugo picard <hugo@hugop>
2022-04-11 14:33:46 +0200

    Modified project.py il compile bien maintenant mais couverture tjrs pas bonne

--- project.py
+++ project.py
@@ -38,16 +38,10 @@ def pricer_1(N, s, rn, hn, bn, f):
 
 
 def f1(x):
-    return max(x-110,0)
+    return np.maximum(x-110,0)
 
-def f2(x):
-    return max(x-100,0)
-
-pricer_1(3,100,0.02,0.05,-0.05,f1)
 pricer_1(20,100,0.02,0.05,-0.05,f1)
 
-pricer_1(20,100,0.02,0.05,-0.05,f2)
-pricer_1(3,100,0.02,0.05,-0.05,f2)
 
 
 
@@ -58,22 +52,22 @@ pricer_1(3,100,0.02,0.05,-0.05,f2)
 ###################################################################
 def pricer_2(N,rn,hn,bn,s,f):
     qn = (rn - bn)/(hn - bn)
-    print("qn = ",qn)
+   # print("qn = ",qn)
 
     St_N = getSt_N(N,hn,bn,s)
-    print("St_N = ",St_N)
+    #print("St_N = ",St_N)
     Vk = f(St_N) #VN
-    print("Vk = ",Vk)
+    #print("Vk = ",Vk)
     for n in range(N,0,-1):
         Vkmoins1 = np.zeros(n)
         for k in range(0,n): #on parcourt Vk
             Vkmoins1[k] = (qn * Vk[k] + (1-qn) * Vk[k+1]) / (1 +rn) # Vkmoins1 = (qn * Vk((1+hn)*St_kmoins1) + (1-qn) * Vk((1+bn)*St_kmoins1) ) / (1 + rn)
 
-        print("St_kmoins1 = ",getSt_N(n-1,hn,bn,s))
-        print("Vkmoins1 = ",Vkmoins1)
+     #   print("St_kmoins1 = ",getSt_N(n-1,hn,bn,s))
+      #  print("Vkmoins1 = ",Vkmoins1)
         Vk = Vkmoins1
-        print("Vk = ",Vk)
-    return Vkmoins1
+       # print("Vk = ",Vk)
+    return Vkmoins1[0]
 
 
 ###################################################################
@@ -104,9 +98,11 @@ pricer_2(3,0.02,0.05,-0.05,100,f)
 
 
 def compare(p1, p2, N, rn, hn, bn, s, f) :
-    return p1(N, rn, hn, bn, s, f) - p2(N, rn, hn, bn, s, f)
+    print (p1(N, s, rn, hn, bn, f))
+    print(p2(N, rn, hn, bn, s, f))
+    return p1(N, s, rn, hn, bn, f) - p2(N, rn, hn, bn, s, f)
 
-compare(pricer_1, pricer_2, np.random.randint(5,16), 0.01, 0.05, -0.05, 100, f1)
+compare(pricer_1, pricer_2, np.random.randint(5,16), 0.01, 0.05, -0.05, 100, f)
 
 
 ###################################################################
