24f4db0cea69484cce48c0fcce599f17ec0a14d4
Damien LE VASSEUR <damien.levasseur@pc130-14.pedago.ensiie.fr>
2022-04-11 16:34:37 +0200

    qmslkdfjmsqlkfdj

--- project.py
+++ project.py
@@ -52,7 +52,7 @@ pricer_1(20,100,0.02,0.05,-0.05,f1)
 ###################################################################
 def pricer_2(N,rn,hn,bn,s,f):
     qn = (rn - bn)/(hn - bn)
-    
+
     St_N = getSt_N(N,hn,bn,s)
     Vk = f(St_N) #VN
     for n in range(N,0,-1):
@@ -60,7 +60,7 @@ def pricer_2(N,rn,hn,bn,s,f):
         for k in range(0,n): #on parcourt Vk
             Vkmoins1[k] = (qn * Vk[k] + (1-qn) * Vk[k+1]) / (1 +rn) # Vkmoins1 = (qn * Vk((1+hn)*St_kmoins1) + (1-qn) * Vk((1+bn)*St_kmoins1) ) / (1 + rn)
         Vk = Vkmoins1
-    return Vkmoins1[0]
+    return Vk[0]
 
 
 ###################################################################
